afecab1b47359cf4e7ef47f09e468a772f8937a4
jotica0602 <alejandroeb5271@gmail.com>
2023-12-23 01:41:47 -0500

    working on parser

--- GeoEngine/Syntax_Analyzer/Parser.cs
+++ GeoEngine/Syntax_Analyzer/Parser.cs
@@ -1,4 +1,17 @@
-public partial class Parser
+namespace ClassLibrary;
+
+public class ASTBuilder
 {
-    
+    int line;
+    List<Token> tokens;
+    int currentTokenIndex;
+    Token currentToken;
+
+    public ASTBuilder(List<Token> tokens)
+    {
+        this.tokens = tokens;
+        currentTokenIndex = 0;
+        currentToken = tokens[currentTokenIndex];
+    }
 }
+
